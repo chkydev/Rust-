bbe0bfba901398425f59d480c9f8f867b11622c4
Rohit <rohit@chky-dev.local>
2026-01-03 23:17:29 +0530

    string

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let mut num:u16=256;
+    let string_literal="hi rust";
+    println!("This is string literal {}",string_literal);
+    /*let mut num:u16=256;
     println!("This is stored in num: {}",num);
     num=199;
-    println!("This is stored in num: {}",num);
+    println!("This is stored in num: {}",num);*/
 }
